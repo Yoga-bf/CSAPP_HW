6192831785d0adefb568f497ce28246d0099db20
Yoga-bf <cbf98012@mail.ustc.edu.cn>
2021-04-16 00:56:43 +0800

    test

--- Eight/tfgets.c
+++ Eight/tfgets.c
@@ -6,7 +6,9 @@ sigjmp_buf buf;
 void handler(int sig) {
     siglongjmp(buf, 1);
 }
-
+//程序运行到tfgets后，会设置一个 sigsetmp，然后通过 alarm 设置一个5秒的时钟
+//然后程序运行到 return fgets，如果5秒后，fgets 没有返回，这个进程就会收到
+//一个 SIGALRM 信号，然后就会执行 handler，进而回到判断语句，返回 NULL
 char* tfgets(char* s, int size, FILE* stream) {
     char* result;
     if (!sigsetjmp(buf, 1)) {
@@ -22,8 +24,8 @@ char* tfgets(char* s, int size, FILE* stream) {
 static int LEN = 100;
 int main()
 {
-    char buf[LEN];
-    char *input = tfgets(buf, LEN, stdin);
+    char buff[LEN];
+    char *input = tfgets(buff, LEN, stdin);
     if (input == NULL) {
         printf("nothing input\n");
     } else {

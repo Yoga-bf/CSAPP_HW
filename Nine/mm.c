f8f917b7862d8687fe230a38b9c57cd41fc4c051
Yoga-bf <cbf98012@mail.ustc.edu.cn>
2021-04-17 15:34:00 +0800

    test

--- Nine/mm.c
+++ Nine/mm.c
@@ -113,3 +113,31 @@ void *mm_malloc(size_t size)
     place(bp, asize);
     return bp;
 }
+
+static void *find_fit(size_t asize)
+{
+    void* bp;
+
+    for (bp = heap_listp; GET_SIZE(bp) > 0; bp = NEXT_BLKP(bp)) {
+        if (!GET_ALLOC(HDRP(bp)) && (GET_SIZE(HDRP(bp)) >= asize)) {
+            return bp;
+        }
+    }
+    return NULL;
+}
+
+static void place(void *bp, size_t asize)
+{
+    size_t csize = GET_SIZE(HDRP(bp));
+    if ((csize - asize) >= (2*DSIZE)) {
+        
+        PUT(HDRP(bp), PACK(asize, 1));
+        PUT(FTRP(bp), PACK(asize, 1));
+        bp = NEXT_BLKP(bp);
+        PUT(FTRP(bp), PACK(csize - asize, 0));
+        PUT(HDRP(bp), PACK(csize - asize, 0));
+    } else {
+        PUT(HDRP(bp), PACK(csize, 1));
+        PUT(FTRP(bp), PACK(csize, 1));
+    }
+}
